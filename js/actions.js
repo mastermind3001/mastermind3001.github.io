a3265c83bbbbc276c8af97c186125de9a0a2b29e
(mastermind3001) <ayderumerov.com@gmail.com>
2023-12-14 11:40:19 +0300

    init

---
+++ js/actions.js
@@ -0,0 +1,30 @@
+const init = () => {
+    let name = document.getElementById('name');
+    let address = document.getElementById('address');
+    let phone = document.getElementById('phone');
+
+
+    // Создаем объект XMLHttpRequest
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "/api/users");
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(JSON.stringify({
+        name: "John Doe" + name.value,
+        address: "Coruscant " + address.value,
+        phone: "R2D2 " + phone.value,
+
+    }));
+
+    window.alert("Спасибо за заказ");
+    console.log({
+        name: "John Doe" + name.value,
+        address: "Coruscant " + address.value,
+        phone: "R2D2 " + phone.value,
+
+    });
+}
+
+document.getElementById('send').addEventListener('click', init);
